95c47c25f09b98361aec5c0777fc5b8ebc38e2df
Brendan Pace <brendanpace99@gmail.com>
2018-10-13 17:52:35 -0500

    Most Updated File
    Character controls

---
+++ 6D/6DSDKUnitySampleApp/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    protected Joystick joystick;
+
+
+	// Use this for initialization
+	void Start () {
+        joystick = FindObjectOfType<Joystick>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(joystick.Horizontal);
+	}
+}
